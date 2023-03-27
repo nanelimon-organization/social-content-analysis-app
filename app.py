ae664109d55d65101d20082195ca53e28ded68a3
Seyma SARIGİL <seymasargil@gmail.com>
2023-03-27 07:34:00 +0300

    feature/new card themas

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    text = '@KullanıcıAdı - hhtps://twitter.com/usr_url_adres'
-    return render_template('index.html', text=text, tarih=datetime.date.today())
+    hashtags= ['turkcell', 'turktelekom']
+    username='@KullanıcıAdı'
+    publish_date= '2023-03-26T21:18:11+00:00'
+
+    return render_template('index.html', hashtags=hashtags, tarih=datetime.date.today(), username=username,publish_date=publish_date)
 
 
 if __name__ == '__main__':
